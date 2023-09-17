f687e3369f6a3729c197b01a21420aa5ae50f06f
Paranyu Akkhraviphanon <paranyuak@gmail.com>
2023-09-17 15:50:17 +0700

    Lots of change Done

--- lib/screens/home/toilet_list_item.dart
+++ lib/screens/home/toilet_list_item.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:public_toilets/models/toilets.dart';
@@ -6,46 +6,40 @@ import 'package:public_toilets/models/toilets.dart';
 class ToiletListItem extends StatelessWidget {
   final Toilet toilet;
 
-  ToiletListItem({
+  const ToiletListItem({
     super.key,
-    required this.toilet
+    required this.toilet,
   });
 
   @override
   Widget build(BuildContext context) {
-    /*List<Widget> star = [] ;
-
-    for(var i=0;i<5;i++){
-      star.add(Icon(Icons.star))
-    }*/
-
-
-
     return Card(
       elevation: 5.0,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.home, size: 30.0,),
+            Icon(Icons.home, size: 30.0),
             Expanded(
-                child: Text(
-                  toilet.name,
-                  style:TextStyle(fontSize: 24.0,color: Colors.red),
-                )
+              child: Text(
+                toilet.name,
+                style: TextStyle(fontSize: 24.0, color: Colors.red),
+              ),
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Row(
                   children: [
-                    if(toilet.point > 3)
+                    // collection if, ไม่ใช่ if statement
+                    if (toilet.point > 2.5)
                       Text('GOOD'),
-                    // collection for ไม่ใช่ for statement
-                    for(var i=0;i<toilet.point.round();i++)
+
+                    // collection for, ไม่ใช่ for statement
+                    for (var i = 0; i < toilet.point.floor(); i++)
                       Icon(Icons.star),
-                    Text(toilet.point.toString()),
+
+                    Text(toilet.point.toString())
                   ],
                 ),
                 Text(toilet.distance.toString())
